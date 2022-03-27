4f6ef0d9124ebe1317ade78387495bc6158fd7de
yportne <yportne@163.com>
2022-03-27 23:23:49 +0800

    fix(wave show):beginning signal value loss

--- front-end/src/wave_show/signal/signal_value.rs
+++ front-end/src/wave_show/signal/signal_value.rs
@@ -46,9 +46,15 @@ impl Component for SignalValue {
         if self.bool_signal {
             let mut points = String::new();
             let mut last: u32 = 0;
+            let mut head: u32 = 0;
+            let mut head_used = false;
             for d in &self.value {
                 let x = ((d.0 as f64) - x_axis)*size;
                 if (0.0..3000.0).contains(&x) {
+                    if !head_used {
+                        points.push_str(&format!("{:.2},{:.2} ", 0, zero_position+(1-head)*40));
+                        head_used = true;
+                    }
                     if d.1 == BigUint::new(vec![1]){
                         points.push_str(&format!("{:.2},{:.2} ", x, zero_position+40));
                         points.push_str(&format!("{:.2},{:.2} ", x, zero_position));
@@ -58,8 +64,16 @@ impl Component for SignalValue {
                         points.push_str(&format!("{:.2},{:.2} ", x, zero_position+40));
                         last = zero_position+40;
                     }
+                }else if d.1 == BigUint::new(vec![1]) {
+                    head = 1;
+                }else {
+                    head = 0;
                 }
             };
+            if !head_used {
+                points.push_str(&format!("{:.2},{:.2} ", 0, zero_position+(1-head)*40));
+                last = zero_position+(1-head)*40;
+            }
             points.push_str(&format!("{:.2},{:.2} ", 3000, last));
 
             html! {
@@ -70,9 +84,23 @@ impl Component for SignalValue {
             let mut points1 = String::new();
             let mut points2 = String::new();
             let mut value: Vec<Html> = vec![];
+            let mut head: BigUint = BigUint::new(vec![0]);
+            let mut head_used = true;
             for d in &self.value {
                 let x = ((d.0 as f64) - x_axis)*size;
                 if (0.0..3000.0).contains(&x) {
+                    if !head_used {
+                        head_used = true;
+                        points1.push_str(&format!("{:.2},{:.2} ", 0, zero_position+40));
+                        points2.push_str(&format!("{:.2},{:.2} ", 0, zero_position));
+                        value.push(
+                            html!{
+                                <text x={format!("{}",0)} y={format!("{}",zero_position+37)} fill="rgb(255,255,255)">
+                                    {format!("{}",head)}
+                                </text>
+                            }
+                        );
+                    }
                     points1.push_str(&format!("{:.2},{:.2} ", x-2.0, zero_position+40));
                     points1.push_str(&format!("{:.2},{:.2} ", x, zero_position+20));
                     points1.push_str(&format!("{:.2},{:.2} ", x+2.0, zero_position+40));
@@ -87,8 +115,22 @@ impl Component for SignalValue {
                             </text>
                         }
                     );
+                }else if x < 0.0 {
+                    head = d.1.clone();
+                    head_used = false;
                 }
             };
+            if !head_used {
+                points1.push_str(&format!("{:.2},{:.2} ", 0, zero_position+40));
+                points2.push_str(&format!("{:.2},{:.2} ", 0, zero_position));
+                value.push(
+                    html!{
+                        <text x={format!("{}",0)} y={format!("{}",zero_position+37)} fill="rgb(255,255,255)">
+                            {format!("{}",head)}
+                        </text>
+                    }
+                );
+            }
             points1.push_str(&format!("{:.2},{:.2} ", 3000, zero_position+40));
             points2.push_str(&format!("{:.2},{:.2} ", 3000, zero_position));
 
