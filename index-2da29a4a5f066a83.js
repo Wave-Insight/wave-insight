a8a12fcaefd59295220b90157e199211c7fcd5b3
yportne13 <yportne13@users.noreply.github.com>
2022-04-20 14:19:40 +0000

    deploy: 04192472394317070d7d72e8c0be4fce26a50d38

--- index-2da29a4a5f066a83.js
+++ index-2da29a4a5f066a83.js
@@ -322,7 +322,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-8d670a731d940dc8_bg.wasm', import.meta.url);
+        input = new URL('index-2da29a4a5f066a83_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
