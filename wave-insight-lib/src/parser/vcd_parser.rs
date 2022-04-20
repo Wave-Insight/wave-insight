04192472394317070d7d72e8c0be4fce26a50d38
yportne <yportne@163.com>
2022-04-20 22:10:23 +0800

    fix(vcd parser):add end clock

--- wave-insight-lib/src/parser/vcd_parser.rs
+++ wave-insight-lib/src/parser/vcd_parser.rs
@@ -7,5 +7,6 @@ pub fn vcd_parser(input: &str, raw_module: &mut Module) -> Module {
     let lines = input.lines();
     let mut state = ParseState{clk: 0, module: raw_module.clone(), stack: vec![]};
     lines.for_each(|l| parsing_line(&mut state, l.to_string()));
+    state.module.end_clock = state.clk;
     state.module
 }
