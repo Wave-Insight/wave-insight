47c042b94cb3f34a1ecfdf46c8e83e5398d2384c
yportne <yportne@163.com>
2022-07-17 12:33:38 +0800

    fix(module value):when get a no data signal

--- wave-insight-lib/src/data_struct/module_value.rs
+++ wave-insight-lib/src/data_struct/module_value.rs
@@ -17,9 +17,13 @@ impl ModuleValue {
     }
     pub fn get(&self, key: &str) -> Vec<(i32,BigUint)> {
         let data = self.value.get(key).unwrap_or(&(Vec::new(),Vec::new())).to_owned();
-        let chunk_size = data.1.len()/data.0.len();
-        let to_big = data.1.chunks(chunk_size).map(BigUint::from_bytes_be);
-        data.0.into_iter().zip(to_big).map(|(l,r)| (l,r)).collect()
+        if data.0.is_empty() {
+            Vec::new()
+        }else {
+            let chunk_size = data.1.len()/data.0.len();
+            let to_big = data.1.chunks(chunk_size).map(BigUint::from_bytes_be);
+            data.0.into_iter().zip(to_big).map(|(l,r)| (l,r)).collect()
+        }
     }
     pub fn new_clk(&mut self, clk: i32) {
         self.clk=clk;
